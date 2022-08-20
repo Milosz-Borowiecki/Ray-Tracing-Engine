628ff5f9991dbdcd2af4fd93530daf93dad6eed3
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2022-08-20 23:36:05 +0200

    hit_record update

--- source/hittable.h
+++ source/hittable.h
@@ -1,12 +1,27 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
+#include <glm/geometric.hpp>
 #include "ray.h"
+#include "rtweekend.h"
+
+class material;
 
 struct hit_record {
     point3 p;
     glm::vec3 normal;
+    shared_ptr<material> mat_ptr;
     float t;
+    bool front_face;
+
+    inline void set_face_normal(const ray& r, glm::vec3& outward_normal) {
+        front_face = glm::dot(r.direction(), outward_normal) < 0.0f;
+        if (front_face) {
+            normal = outward_normal;
+        } else {
+            normal = -outward_normal;
+        }
+    }
 };
 
 class hittable {
