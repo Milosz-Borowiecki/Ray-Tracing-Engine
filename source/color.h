39cd7bfa3f4fe8eb9207b2e5fe8259f486deb597
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2022-08-21 16:16:52 +0200

    update write_color function

--- source/color.h
+++ source/color.h
@@ -5,21 +5,20 @@
 
 #include <iostream>
 
-void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
-    auto r = pixel_color.x;
-    auto g = pixel_color.y;
-    auto b = pixel_color.z;
+void write_color(std::ostream &out,const color& pixel_color, int samples_per_pixel) {
 
     // Divide the color by the number of samples and gamma-correct for gamma=2.0.
     auto scale = 1.0f / samples_per_pixel;
-    r = sqrt(scale * r);
-    g = sqrt(scale * g);
-    b = sqrt(scale * b);
+    glm::vec3 color = glm::clamp(
+        glm::vec3(sqrt(scale * pixel_color.x),sqrt(scale * pixel_color.y),sqrt(scale * pixel_color.z)),
+        glm::vec3(0.0f),
+        glm::vec3(0.999f)
+        );
 
     // Write the translated [0,255] value of each color component.
-    out << static_cast<int>(256 * clamp(r, 0.0f, 0.999f)) << ' '
-        << static_cast<int>(256 * clamp(g, 0.0f, 0.999f)) << ' '
-        << static_cast<int>(256 * clamp(b, 0.0f, 0.999f)) << '\n';
+    out << static_cast<int>(256 * color.x) << ' '
+        << static_cast<int>(256 * color.y) << ' '
+        << static_cast<int>(256 * color.z) << '\n';
 }
 
 #endif
