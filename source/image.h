8b67b4d422e7e3c37b9163d8d7322e045f468b15
Milosz-Borowiecki <miloszborowiecki2@gmail.com>
2023-10-08 01:05:08 +0200

    Rebuild image.h and change static to inline in vec

--- source/image.h
+++ source/image.h
@@ -3,25 +3,35 @@
 
 #include "pixel.h"
 
-template <size_t array_size>
 class image {
+
     public:
-        std::unique_ptr<std::array<uint8_t, array_size>> color_image = std::make_unique<std::array<uint8_t, array_size>>();
-        std::unique_ptr<std::array<uint8_t, array_size>> albedo_image = std::make_unique<std::array<uint8_t, array_size>>();
-        std::unique_ptr<std::array<uint8_t, array_size>> normal_image = std::make_unique<std::array<uint8_t, array_size>>();
+        image(int p_width,int p_height,int p_channels): width(p_width), height(p_height), channels(p_channels){
+            int size = channels * width * height;
+            color_image.resize(size);
+            albedo_image.resize(size);
+            normal_image.resize(size);
+        }
         
         void save_pixel_data(const pixel_data& data,uint32_t index){
-            color_image->data()[index] = static_cast<uint8_t>(data.color.x);
-            albedo_image->data()[index] = static_cast<uint8_t>(data.albedo.x);
-            normal_image->data()[index] = static_cast<uint8_t>(data.normal.x);
+            color_image.at(index) = static_cast<uint8_t>(data.color.x);
+            albedo_image.at(index) = static_cast<uint8_t>(data.albedo.x);
+            normal_image.at(index) = static_cast<uint8_t>(data.normal.x);
             index += 1;
-            color_image->data()[index] = static_cast<uint8_t>(data.color.y);
-            albedo_image->data()[index] = static_cast<uint8_t>(data.albedo.y);
-            normal_image->data()[index] = static_cast<uint8_t>(data.normal.y);
+            color_image.at(index) = static_cast<uint8_t>(data.color.y);
+            albedo_image.at(index) = static_cast<uint8_t>(data.albedo.y);
+            normal_image.at(index) = static_cast<uint8_t>(data.normal.y);
             index += 1;
-            color_image->data()[index] = static_cast<uint8_t>(data.color.z);
-            albedo_image->data()[index] = static_cast<uint8_t>(data.albedo.z);
-            normal_image->data()[index] = static_cast<uint8_t>(data.normal.z);
+            color_image.at(index) = static_cast<uint8_t>(data.color.z);
+            albedo_image.at(index) = static_cast<uint8_t>(data.albedo.z);
+            normal_image.at(index) = static_cast<uint8_t>(data.normal.z);
         }
+        
+        int channels;
+        int width;
+        int height;
+        std::vector<uint8_t> color_image;
+        std::vector<uint8_t> albedo_image;
+        std::vector<uint8_t> normal_image;
 };
 #endif
--- source/vec3.h
+++ source/vec3.h
@@ -16,7 +16,7 @@ inline glm::vec3 random(float min, float max) {
     return glm::vec3(random_float(min, max), random_float(min, max), random_float(min, max));
 }
 
-static glm::vec3 random_in_unit_sphere() {
+inline glm::vec3 random_in_unit_sphere() {
     while (true) {
         const auto p = random(-1.0f, 1.0f);
         if (glm::dot(p, p) >= 1) {
@@ -28,27 +28,27 @@ static glm::vec3 random_in_unit_sphere() {
     }
 }
 
-static glm::vec3 random_unit_vector() {
+inline glm::vec3 random_unit_vector() {
     return glm::normalize(random_in_unit_sphere());
 }
 
-static bool near_zero(glm::vec3 e) {
+inline bool near_zero(glm::vec3 e) {
     const auto s = 1e-8;
     return (fabs(e.x) < s) && (fabs(e.y) < s) && (fabs(e.z) < s);
 }
 
-static glm::vec3 reflect(const glm::vec3& v, const glm::vec3& n) {
+inline glm::vec3 reflect(const glm::vec3& v, const glm::vec3& n) {
     return v - 2 * glm::dot(v, n) * n;
 }
 
-static glm::vec3 refract(const glm::vec3& uv, const glm::vec3& n, float etai_over_etat) {
+inline glm::vec3 refract(const glm::vec3& uv, const glm::vec3& n, float etai_over_etat) {
     const float cos_theta = fmin(glm::dot(-uv, n), 1.0f);
     const glm::vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
     const glm::vec3 r_out_parallel = -sqrt(fabs(1.0f - glm::dot(r_out_perp, r_out_perp))) * n;
     return r_out_perp + r_out_parallel;
 }
 
-static glm::vec3 random_in_unit_disk() {
+inline glm::vec3 random_in_unit_disk() {
     while (true) {
         const auto p = glm::vec3(random_float(-1, 1), random_float(-1, 1), 0);
         if (glm::dot(p, p) >= 1) {
